5681316aefd33cf3a8126c3155e6afe5e5066d50
Tu <Tu@INSPIRON-7000>
2018-06-11 23:35:05 +0700

    + Fix minor bugs

---
+++ src/SearchEngine/Helper.java
@@ -0,0 +1,268 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package SearchEngine;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import org.apache.poi.EncryptedDocumentException;
+import org.apache.poi.hwpf.HWPFDocument;
+import org.apache.poi.hwpf.extractor.WordExtractor;
+import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
+import org.apache.poi.ss.usermodel.*;
+import org.apache.poi.xwpf.usermodel.XWPFDocument;
+import org.apache.poi.xwpf.usermodel.XWPFParagraph;
+
+/**
+ *
+ * @author Tu
+ */
+public class Helper {
+
+    public static Helper helper = null;
+    public List<Integer> dataIndex = null;
+    String[] fileNames = {"a-d", "e-h", "i-l", "m-p", "q-t", "u-w", "x-z", "other"};
+    List<BufferedWriter> bufferList;
+    List<FileWriter> writerList;
+
+    public String[] readDocFile(String filePath) {
+        File file = null;
+        WordExtractor extractor = null;
+        String[] results = null;
+        try {
+
+            file = new File(filePath);
+            FileInputStream fis = new FileInputStream(file.getAbsolutePath());
+            HWPFDocument document = new HWPFDocument(fis);
+            extractor = new WordExtractor(document);
+            results = extractor.getParagraphText();
+        } catch (Exception exep) {
+            exep.printStackTrace();
+        }
+
+        return results;
+    }
+
+    public String[] readDocxFile(String filePath) {
+        File file = null;
+        String[] results = null;
+        try {
+
+            file = new File(filePath);
+            FileInputStream fis = new FileInputStream(file.getAbsolutePath());
+            XWPFDocument document = new XWPFDocument(fis);
+            List<XWPFParagraph> paragraphs = document.getParagraphs();
+            results = new String[paragraphs.size()];
+            for (int i = 0; i < paragraphs.size(); i++) {
+
+                results[i] = paragraphs.get(i).getText();
+            }
+        } catch (Exception exep) {
+            exep.printStackTrace();
+        }
+        return results;
+    }
+
+    public String[] readExcelFile(String filePath) {
+        Workbook workbook = null;
+        String[] results = null;
+        try {
+            workbook = WorkbookFactory.create(new File(filePath));
+
+            System.out.println("Workbook has " + workbook.getNumberOfSheets() + " Sheets : ");
+            Iterator<Sheet> sheetIterator = workbook.sheetIterator();
+            List<String> data = new ArrayList<>();
+            dataIndex = new ArrayList<>();
+
+            while (sheetIterator.hasNext()) {
+                Sheet sheet = sheetIterator.next();
+                DataFormatter dataFormatter = new DataFormatter();
+                Iterator<Row> rowIterator = sheet.rowIterator();
+                while (rowIterator.hasNext()) {
+                    Row row = rowIterator.next();
+                    Iterator<Cell> cellIterator = row.cellIterator();
+
+                    while (cellIterator.hasNext()) {
+                        Cell cell = cellIterator.next();
+                        String cellValue = dataFormatter.formatCellValue(cell);
+                        data.add(cellValue);
+                        dataIndex.add(workbook.getSheetIndex(sheet));
+                    }
+                }
+            }
+            workbook.close();
+
+            results = new String[data.size()];
+            for (int i = 0; i < data.size(); i++) {
+                results[i] = data.get(i);
+            }
+        } catch (IOException ex) {
+            Logger.getLogger(Helper.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (InvalidFormatException ex) {
+            Logger.getLogger(Helper.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (EncryptedDocumentException ex) {
+            Logger.getLogger(Helper.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return results;
+    }
+
+    private String[] readTxtFile(String filePath) {
+        String[] results = null;
+        try {
+            FileInputStream fis = new FileInputStream(filePath);
+            BufferedReader br = new BufferedReader(new InputStreamReader(fis));
+            List<String> data = new ArrayList<>();
+            String line = "";
+            while ((line = br.readLine()) != null) {
+                data.add(line);
+            }
+            results = new String[data.size()];
+            for (int i = 0; i < data.size(); i++) {
+                results[i] = data.get(i);
+            }
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(Helper.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+            Logger.getLogger(Helper.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return results;
+    }
+
+    public static Helper getInstance() {
+        if (helper == null) {
+            helper = new Helper();
+        }
+        return helper;
+    }
+
+    public int getSheetByIndex(int index) {
+        return dataIndex.get(index);
+    }
+
+    public String[] readFileByExtenstions(String filePath) {
+        if (filePath.endsWith("doc")) {
+            return readDocFile(filePath);
+        }
+        if (filePath.endsWith("docx")) {
+            return readDocxFile(filePath);
+        }
+        if (filePath.endsWith("xls") || filePath.endsWith("xlsx")) {
+            return readExcelFile(filePath);
+        }
+        if (filePath.endsWith("txt")) {
+            return readTxtFile(filePath);
+        }
+        return null;
+    }
+
+    public boolean checkExcelExtention(String filePath) {
+        return filePath.endsWith("xls") || filePath.endsWith("xlsx");
+    }
+
+    public List<BufferedWriter> openFile() {
+        writerList = new ArrayList<>();
+        bufferList = new ArrayList<>();
+        try {
+            for (int i = 0; i < fileNames.length; i++) {
+                FileWriter fileWriter = new FileWriter("indexed/" + fileNames[i] + ".txt", true);
+                writerList.add(fileWriter);
+            }
+        } catch (IOException ex) {
+            Logger.getLogger(Helper.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return bufferList;
+    }
+
+    public void closeFile() {
+
+        try {
+            for (BufferedWriter buffer : bufferList) {
+                buffer.close();
+            }
+            for (FileWriter writer : writerList) {
+                writer.close();
+            }
+        } catch (IOException ex) {
+            Logger.getLogger(Helper.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    public void flushFile() {
+
+        try {
+            for (BufferedWriter buffer : bufferList) {
+                buffer.flush();
+            }
+            for (FileWriter writer : writerList) {
+                writer.flush();
+            }
+        } catch (IOException ex) {
+            Logger.getLogger(Helper.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    public BufferedWriter checkDistributionRange(String term) {
+        // áàảãạăắặằẳẵâấầẩẫậđéèẻẽẹêếềểễệíìỉĩịóòỏõọôốồổỗộơớờởỡợúùủũụưứừửữựýỳỷỹỵÁÀẢÃẠĂẶẰẲẴÂẤẦẨẪẬĐÉÈẺẼẸÊẾỀỂỄỆÍÌỈĨỊÓÒỎÕỌÔỐỒỔỖỘƠỚỜỞỠỢÚÙỦŨỤƯỨỪỬỮỰÝỲỶỸỴ
+        try {
+
+            Pattern pattern = Pattern.compile("^[aáàảãạăắặằẳẵâấầẩẫậbcdđAÁÀẢÃẠĂẶẰẲẴÂẤẦẨẪBCD]");
+            BufferedWriter bufferedWriter = null;
+            Matcher m = pattern.matcher(term);
+            if (m.find()) {
+                return bufferedWriter = new BufferedWriter(new FileWriter("indexed/" + fileNames[0] + ".txt", true));
+
+            }
+            pattern = Pattern.compile("^[eéèẻẽẹêếềểễệfghEÉÈẺẼẸÊẾỀỂỄỆFGH]");
+            m = pattern.matcher(term);
+            if (m.find()) {
+                return bufferedWriter = new BufferedWriter(new FileWriter("indexed/" + fileNames[1] + ".txt", true));
+            }
+            pattern = Pattern.compile("^[iíìỉĩịjklIÍÌỈĨỊJKL]");
+            m = pattern.matcher(term);
+            if (m.find()) {
+                return bufferedWriter = new BufferedWriter(new FileWriter("indexed/" + fileNames[2] + ".txt", true));
+            }
+            pattern = Pattern.compile("^[mnoóòỏõọôốồổỗộơớờởỡợpMNOÓÒỎÕỌÔỐỒỔỖỘƠỚỜỞỠỢ]");
+            m = pattern.matcher(term);
+            if (m.find()) {
+                return bufferedWriter = new BufferedWriter(new FileWriter("indexed/" + fileNames[3] + ".txt", true));
+            }
+            pattern = Pattern.compile("^[qrstQRST]");
+            m = pattern.matcher(term);
+            if (m.find()) {
+                return bufferedWriter = new BufferedWriter(new FileWriter("indexed/" + fileNames[4] + ".txt", true));
+            }
+            pattern = Pattern.compile("^[uúùủũụưứừửữựvwUÚÙỦŨỤƯỨỪỬỮỰVW]");
+            m = pattern.matcher(term);
+            if (m.find()) {
+                return bufferedWriter = new BufferedWriter(new FileWriter("indexed/" + fileNames[5] + ".txt", true));
+            }
+            pattern = Pattern.compile("^[xyýỳỷỹỵzXYÝỲỶỸỴZ]");
+            m = pattern.matcher(term);
+            if (m.find()) {
+                return bufferedWriter = new BufferedWriter(new FileWriter("indexed/" + fileNames[6] + ".txt", true));
+            }
+            return bufferedWriter = new BufferedWriter(new FileWriter("indexed/" + fileNames[7] + ".txt", true));
+        } catch (IOException ex) {
+            Logger.getLogger(Helper.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return null;
+    }
+}
